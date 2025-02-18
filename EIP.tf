b45f62020e8e4c85a467e2a913e6a75b471d4118
amgadelhosieny <139463395+amgadelhosieny@users.noreply.github.com>
2025-02-18 22:06:28 +0200

    EIP.tf

--- EIP.tf
+++ EIP.tf
@@ -1,7 +1,20 @@
-resource "aws_eip" "main-nat-EIP" {
-    domain = "vpc"
+resource "aws_eip" "main-Bastion-EIP" {
+    instance = aws_instance.main-Bastion-host.id
+
 }
 
+resource "aws_eip" "main-Nginx-A-EIP" {
+    instance = aws_instance.main-Nginx-A.id
+
+}
+
+resource "aws_eip" "main-Nginx-B-EIP" {
+    instance = aws_instance.main-Nginx-B.id
+
+}
+
+resource "aws_eip" "main-nat-EIP" {
+}
 
 output "bastion_private_ip" {
     value = aws_instance.main-Bastion-host.private_ip
